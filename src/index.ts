b17cd950749ce3c1d5810d0da67b3ba385190391
rkubapl <39430751+rkubapl@users.noreply.github.com>
2021-05-12 11:26:52 +0200

    yoink

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import RPCHandler from './handlers/RPCHandler';
 import get from 'axios';
 const RPHandler = new RPCHandler();
 setInterval(async () => {
-  const data = await get('https://api.mcsrvstat.us/2/papugoland.me');
+  const data = await get('https://api.mcsrvstat.us/2/kaczkoland.pl');
   if (RPHandler.ready) {
     const rp = RPHandler.parseToRP(data.data);
     // @ts-ignore
