d02faae9e58e0ddd4c197c1eceacee6a35b41877
rkubapl <39430751+rkubapl@users.noreply.github.com>
2021-05-13 08:46:03 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import RPCHandler from './handlers/RPCHandler';
 import get from 'axios';
+import util from 'minecraft-server-util';
+
 const RPHandler = new RPCHandler();
 
-const seconds = 60;
+const seconds = 10;
 
 setInterval(async () => {
-  const data = await get('https://api.mcsrvstat.us/2/kaczkoland.pl');
-  if (RPHandler.ready) {
-    const rp = RPHandler.parseToRP(data.data);
+  const status = await util.query('kaczkoland.pl', { port: 25565 });
+  if (RPHandler.ready && status !== undefined) {
+    const rp = RPHandler.parseToRP(status);
     // @ts-ignore
     RPHandler.setRP(rp);
   }
